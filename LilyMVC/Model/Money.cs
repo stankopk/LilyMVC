37150fb998455e5badfb317918248b7c0a180c37
Stanko Yanev <stanko.yanev@edu.mon.bg>
2023-10-10 15:28:18 +0300

    Add project files.

---
+++ LilyMVC/Model/Money.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilyMVC.Model
+{
+    internal class Money
+    {
+    }
+}
