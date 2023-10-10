633450de954d663639f83938c698dc8e7a4aa7c3
DanailDanev <PC03-204@DESKTOP-V4KD07J>
2023-10-10 16:36:44 +0300

    MADE THE WHOLE THANNG YAAMEAN

--- LilyMVC/Views/MainView.cs
+++ LilyMVC/Views/MainView.cs
@@ -8,5 +8,47 @@ namespace LilyMVC.Views
 {
     internal class MainView
     {
+        public class Display
+        {
+             
+            /// Сума на сметката         
+            public double Amount { get; set; }
+
+            /// Процент за бакшиш           
+            public double Percent { get; set; }
+  
+            /// Обща сума за плащане
+            public double Total { get; set; }
+            
+            /// Сума за бакшиша         
+            public double TipAmount { get; set; }
+     
+            /// Конструктор        
+            public Display()
+            {
+                Amount = 0;
+                Percent = 0;
+                Total = 0;
+                TipAmount = 0;
+                GetValues();
+            }
+            
+            /// Четене на входни данни от конзолата 
+            public void GetValues()
+            {
+                Console.WriteLine("Enter the amount of the meal: ");
+                this.Amount = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the percent you want to tip: ");
+                this.Percent = double.Parse(Console.ReadLine());
+            }
+    
+            /// Отпечатва изходни данни на конзолата 
+            public void ShowTipAndTotal()
+            {
+                Console.WriteLine("You tip is: {0:C}", TipAmount);
+                Console.WriteLine("The total will be {0:C}", Total);
+            }
+        }
     }
 }
